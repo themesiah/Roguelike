1852467758a28ad3b561b4df8f7f8c0a6ba61a11
Marc Palenzuela <marc.palenzuela.reyes@gmail.com>
2021-04-07 13:03:02 +0200

    Camera shake first version. Shield effect HDR. Float game event.

---
+++ Assets/02. Scripts/Behaviours/Camera/CinemachineShake.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+namespace Laresistance.Behaviours
+{
+    public class CinemachineShake : MonoBehaviour
+    {
+        [SerializeField]
+        private CinemachineVirtualCamera vcam = default;
+
+		[Header("Trauma properties")]
+		[SerializeField]
+		private float traumaDecreaseRate = 0.3f;
+		[SerializeField]
+		private float traumaForce = 2f;
+
+		private float trauma = 0f;
+		private CinemachineBasicMultiChannelPerlin camNoise;
+
+        private void Awake()
+        {
+			camNoise = vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+			if (camNoise != null)
+			{
+				camNoise.m_AmplitudeGain = 0f;
+			}
+		}
+
+        void LateUpdate()
+		{
+			if (trauma > 0f && traumaDecreaseRate > 0f)
+			{
+				float t = Mathf.Max(0f, trauma - traumaDecreaseRate * Time.deltaTime);
+				ChangeTrauma(t);
+			}
+		}
+
+		public void AddTrauma(float t)
+		{
+			t = Mathf.Min(trauma + t, 1f);
+			ChangeTrauma(t);
+		}
+
+		private void ChangeTrauma(float t)
+        {
+			trauma = t;
+			if (camNoise != null)
+			{
+				camNoise.m_AmplitudeGain = t * traumaForce;
+			}
+		}
+	}
+}
