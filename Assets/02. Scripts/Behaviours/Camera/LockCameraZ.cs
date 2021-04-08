a346097e47a622515e0fe24ae9e789782c1f381d
Marc Palenzuela <marc.palenzuela.reyes@gmail.com>
2021-04-08 18:06:34 +0200

    Bloquear cámaras de cinemachine en eje Z.

---
+++ Assets/02. Scripts/Behaviours/Camera/LockCameraZ.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+namespace Laresistance.Behaviours
+{
+    public class LockCameraZ : CinemachineExtension
+    {
+        [SerializeField] [Tooltip("Lock the camera's Z position to this value")]
+        private float zPosition = -10f;
+        protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+        {
+            if (stage == CinemachineCore.Stage.Body)
+            {
+                var pos = state.RawPosition;
+                pos.z = zPosition;
+                state.RawPosition = pos;
+
+            }
+        }
+    }
+}
