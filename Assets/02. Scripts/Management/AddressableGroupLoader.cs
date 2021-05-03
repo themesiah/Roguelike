03befec9d940eb2a2be586b0c782ff81085e75b4
Marc Palenzuela <marc.palenzuela.reyes@gmail.com>
2021-05-03 16:11:16 +0200

    Empezado tema addressables (WIP TOTAL)

---
+++ Assets/02. Scripts/Management/AddressableGroupLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Laresistance.Management
+{
+    public class AddressableGroupLoader : MonoBehaviour
+    {
+        [SerializeField]
+        private string groupName = "";
+
+        [SerializeField]
+        private bool loadOnStart = false;
+
+        private void Start()
+        {
+            if (loadOnStart)
+            {
+                LoadGroup();
+            }
+        }
+
+        public void LoadGroup()
+        {
+            StartCoroutine(LoadGroupCoroutine());
+        }
+
+        public IEnumerator LoadGroupCoroutine()
+        {
+            yield return Addressables.LoadAssetsAsync<GameObject>(groupName, (go) => {
+                Debug.LogFormat("{0} loaded!", go.name);
+            });
+        }
+    }
+}
